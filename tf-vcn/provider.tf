3d575f74ec485b5548785a7375e1ecf4aa7f3c04
mnogueiraops <mvjnogueira@gmail.com>
2023-05-16 23:31:19 -0300

    Adicionando modulo "VCN"

---
+++ tf-vcn/provider.tf
@@ -0,0 +1,7 @@
+provider "oci" {
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaadbs6vkmvt6tcu5ndwabml7q3b6lh34i27yw6a74ahmsmd57lotva"
+  user_ocid = "ocid1.user.oc1..aaaaaaaa5wlldyws6jewhuxiyt52wt2kktmexjd3oki5iyyreqazudds54fq" 
+  private_key_path = "/home/devops/Downloads/private-terraform.pem"
+  fingerprint = "ab:be:44:39:48:f9:6f:53:d5:38:0e:f6:59:75:79:08"
+  region = "sa-vinhedo-1"
+}
---
+++ tf-vcn/vcn-module.tf
@@ -0,0 +1,80 @@
+# Source from https://registry.terraform.io/modules/oracle-terraform-modules/vcn/oci/
+module "vcn"{
+  source  = "oracle-terraform-modules/vcn/oci"
+  version = "3.1.0"
+  # insert the 5 required variables here
+
+  # Required Inputs
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaaeji7lmv6yrejwphcmajsuq5vabnrwpf474nctns5wu3pgsgyxukq"
+  region = "sa-vinhedo-1"
+
+  internet_gateway_route_rules = null
+  local_peering_gateways = null
+  nat_gateway_route_rules = null
+
+  # Optional Inputs
+  vcn_name = "vcn-terraform"
+  vcn_dns_label = "vcnterraform"
+  vcn_cidrs = ["10.3.0.0/16"]
+  
+  create_internet_gateway = true
+  create_nat_gateway = true
+  create_service_gateway = true  
+}
+
+# Source from https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/core_subnet
+
+resource "oci_core_subnet" "vcn-private-subnet"{
+
+  # Required
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaaeji7lmv6yrejwphcmajsuq5vabnrwpf474nctns5wu3pgsgyxukq"
+  vcn_id = module.vcn.vcn_id
+  cidr_block = "10.3.1.0/24"
+ 
+  # Optional
+  # Caution: For the route table id, use module.vcn.nat_route_id.
+  # Do not use module.vcn.nat_gateway_id, because it is the OCID for the gateway and not the route table.
+  route_table_id = module.vcn.nat_route_id
+  security_list_ids = [oci_core_security_list.private-security-list.id]
+  display_name = "private-subnet"
+}
+
+# Source from https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/core_subnet
+
+resource "oci_core_subnet" "vcn-public-subnet"{
+
+  # Required
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaaeji7lmv6yrejwphcmajsuq5vabnrwpf474nctns5wu3pgsgyxukq"
+  vcn_id = module.vcn.vcn_id
+  cidr_block = "10.3.2.0/24"
+ 
+  # Optional
+  route_table_id = module.vcn.ig_route_id
+  security_list_ids = [oci_core_security_list.public-security-list.id]
+  display_name = "public-subnet"
+}
+
+# Source from https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/core_security_list
+
+resource "oci_core_security_list" "private-security-list"{
+
+# Required
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaaeji7lmv6yrejwphcmajsuq5vabnrwpf474nctns5wu3pgsgyxukq"
+  vcn_id = module.vcn.vcn_id
+
+# Optional
+  display_name = "security-list-for-private-subnet"
+}
+
+# Source from https://registry.terraform.io/providers/oracle/oci/latest/docs/resources/core_security_list
+
+resource "oci_core_security_list" "public-security-list"{
+
+# Required
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaaeji7lmv6yrejwphcmajsuq5vabnrwpf474nctns5wu3pgsgyxukq"
+  vcn_id = module.vcn.vcn_id
+
+# Optional
+  display_name = "security-list-for-public-subnet"
+
+}
