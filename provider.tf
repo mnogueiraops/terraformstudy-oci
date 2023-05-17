061a7cd1c9a6c8147df5da8ef63f30b19feb8da6
mnogueiraops <mvjnogueira@gmail.com>
2023-05-16 23:13:16 -0300

    Adicionando tf-compartment.tf para criação de compartment no OCI

--- provider.tf
+++ provider.tf
@@ -3,5 +3,5 @@ provider "oci" {
   user_ocid = "ocid1.user.oc1..aaaaaaaa5wlldyws6jewhuxiyt52wt2kktmexjd3oki5iyyreqazudds54fq" 
   private_key_path = "/home/devops/Downloads/private-terraform.pem"
   fingerprint = "ab:be:44:39:48:f9:6f:53:d5:38:0e:f6:59:75:79:08"
-  region = "sa-saopaulo-1"
+  region = "sa-vinhedo-1"
 }
