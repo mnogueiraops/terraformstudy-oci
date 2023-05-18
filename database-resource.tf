2783bb246575044b7acaceaf3ea81a989687ac85
mnogueiraops <mvjnogueira@gmail.com>
2023-05-18 00:45:48 -0300

    Solving typos

--- database-resource.tf
+++ database-resource.tf
@@ -1,13 +1,13 @@
 # Define the MySQL DB System
 resource "oci_mysql_mysql_db_system" "example" {
-  compartment_id       = "ocid1.tenancy.oc1..aaaaaaaadbs6vkmvt6tcu5ndwabml7q3b6lh34i27yw6a74ahmsmd57lotva"
-  availability_domain = "WRCw:SA-VINHEDO-1-AD-1"
-  subnet_id            = "ocid1.subnet.oc1.sa-vinhedo-1.aaaaaaaacselprqf5azedcgnt5az2grkajndr66uwu4aiq4pfg55v7u5y3nq"
-  display_name         = "terraform-mysql-dbsystem"
-  shape_name           = "MySQL.VM.Standard.E3.1.8GB"
-  hostname_label       = "terraform-mysql"
-  data_storage_size_in_gb = 256
-  admin_username        = "admin"
-  admin_password        = "MySQLP@SS13576"
+  compartment_id       = var.compartment_id
+  availability_domain = var.availability_domain
+  subnet_id            = var.subnet_id
+  display_name         = var.display_name
+  shape_name           = var.mysql_shape_name
+  hostname_label       = var.mysql_db_system_hostname_label
+  data_storage_size_in_gb = var.mysql_db_system_data_storage_size_in_gb
+  admin_username        = var.mysql_db_system_admin_username
+  admin_password        = var.mysql_db_system_admin_password
 }
 
